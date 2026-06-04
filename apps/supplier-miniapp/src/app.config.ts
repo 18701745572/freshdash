ddce6de6903417022f5661c9adc3fb1f686800f9
18701745572 <jianghuizhong18701745572@gmail.com>
2026-06-05 06:51:39 +0800

    feat: 初始化供应商端小程序 (#4)
    - 创建 Taro 项目结构与页面路由
    
    - 实现订单列表、订单详情、发货页面

---
+++ apps/supplier-miniapp/src/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  pages: [
+    'pages/order-list/index',
+    'pages/order-detail/index',
+    'pages/ship/index',
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: '鲜达供应商',
+    navigationBarTextStyle: 'black',
+  },
+})
---
+++ apps/supplier-miniapp/src/app.ts
@@ -0,0 +1,8 @@
+import React, { PropsWithChildren } from 'react'
+import './app.scss'
+
+function App({ children }: PropsWithChildren<any>) {
+  return children
+}
+
+export default App
